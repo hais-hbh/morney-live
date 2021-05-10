78abd66e8c56ab4da9e8d7570106479cece96843
hais-hbh <578092215@qq.com>
2021-05-10 21:59:36 +0800

    deploy

---
+++ precache-manifest.ffdd4e8d2897b431c1259e454f5abdfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a2770fe437facc7d192",
+    "url": "/morney-live/css/app.682e195b.css"
+  },
+  {
+    "revision": "8f1e15e242d90a68ee16d39d3cc3cc73",
+    "url": "/morney-live/index.html"
+  },
+  {
+    "revision": "5a2770fe437facc7d192",
+    "url": "/morney-live/js/app.2820de32.js"
+  },
+  {
+    "revision": "b2b89b99e7b772404a45",
+    "url": "/morney-live/js/chunk-vendors.a1af04e7.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-live/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-live/robots.txt"
+  }
+]);
